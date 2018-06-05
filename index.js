ca851bf7b6d82dba1e5dfb2b22011f22ee97a046
Will <farleywa@bc.edu>
2018-06-05 09:15:15 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,64 @@
+// This is an attendance app for Alexa
+// Initially it will just read off a list of names
+
+'use strict';
+const Alexa = require("alexa-sdk");
+const AWS = require("aws-sdk");
+
+AWS.config.update({region: 'us-east-1'});
+
+exports.handler = function ((event, context, callback)) {
+    const alexa = Alexa.handler(event, context);
+
+    alexa.appId = 'TBD';
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'LaunchRequest': function () {
+        console.log('LaunchRequest');
+
+        const speechOutput = 'This is the Roll Call skill.';
+        const errorOutput = 'Whoops! Something went wrong!';
+
+        //do you need double quotes for JSON?
+        this.context.succeed ({
+            'response': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': speechOutput
+                },
+                'shouldEndSession': true
+            },
+            'sessionAttributes': {}
+        });
+        //should there be an error/failure case?
+    },
+
+    //Required Intents
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = 'This is the Roll Call skill.';
+
+        this.response.speak(speechOutput);
+        this.emit(':responseReady'); //what is this?
+    },
+
+    'AMAZON.CancelIntent': function () {
+        const speechOutput = 'Goodbye!';
+
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    },
+
+    'AMAZON.StopIntent': function () {
+        const speechOutput = 'See you later!';
+
+        this.response.speak(speechOutput);
+        this.emit(':responseReady');
+    }//,
+
+
+    //Custom Intents
+
+};
