e042a3ce8af6777a43a38a14863aafb80e3e7d72
Will <farleywa@bc.edu>
2018-06-06 10:15:23 -0400

    HashMap

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Alexa = require("alexa-sdk");
 const AWS = require("aws-sdk");
 const config = require("./user-config.json");
 
-var courses = ["1111": [{name: "Tom", beenCalled: 0}, {name: "Jerry", beenCalled: 0}, {name: "Joe", beenCalled: 0}],
-    "2222": [{name: "Jack", beenCalled: 0}, {name: "Daewoo", beenCalled: 0}]];
+var courses = new HashMap(key: "1111", value: [{name: "Tom", beenCalled: 0}, {name: "Jerry", beenCalled: 0}, {name: "Joe", beenCalled: 0}],
+    key: "2222", value: [{name: "Jack", beenCalled: 0}, {name: "Daewoo", beenCalled: 0}]);
 
 AWS.config.update({region: 'us-east-1'});
 
